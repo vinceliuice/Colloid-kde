059532b346f8249a262601d8887b5ed708c0b886
vinceliuice <vinceliuice@hotmail.com>
2022-05-23 15:14:44 +0800

    Add nord version

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ mkdir -p                                                                        
 
 install() {
   local name=${1}
-  local theme=${2}
-  local color=${3}
+  local color=${2}
+  local theme=${3}
 
   [[ ${color} == '-dark' ]] && local ELSE_COLOR='Dark'
   [[ ${color} == '-light' ]] && local ELSE_COLOR='Light'
@@ -35,7 +35,7 @@ install() {
 
   local AURORAE_THEME="${AURORAE_DIR}/${name}${color}"
   local PLASMA_THEME="${PLASMA_DIR}/${name}${theme}${color}"
-  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${theme}${color}"
+  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color}${theme}"
   local SCHEMES_THEME="${SCHEMES_DIR}/${name}${ELSE_COLOR}${ELSE_THEME}.colors"
   local KVANTUM_THEME="${KVANTUM_DIR}/${name}${ELSE_THEME}"
   local WALLPAPER_THEME="${WALLPAPER_DIR}/${name}${theme}${color}"
@@ -49,28 +49,27 @@ install() {
   [[ -d ${WALLPAPER_THEME} ]] && rm -rf ${WALLPAPER_THEME}
   [[ -f ${LATTE_THEME} ]] && rm -rf ${LATTE_THEME}
 
-  cp -r ${SRC_DIR}/aurorae/${name}${color}                                                   ${AURORAE_DIR}
-  cp -r ${SRC_DIR}/aurorae/${name}${color}-round                                             ${AURORAE_DIR}
-  cp -r ${SRC_DIR}/aurorae/common/*                                                          ${AURORAE_DIR}/${name}${color}
-  cp -r ${SRC_DIR}/aurorae/common/*                                                          ${AURORAE_DIR}/${name}${color}-round
-  cp -r ${SRC_DIR}/color-schemes/${name}${ELSE_THEME}${ELSE_COLOR}.colors                    ${SCHEMES_DIR}
+  cp -r ${SRC_DIR}/aurorae/${name}${color}${theme}                                           ${AURORAE_DIR}
+  cp -r ${SRC_DIR}/aurorae/${name}${color}-round${theme}                                     ${AURORAE_DIR}
+  cp -r ${SRC_DIR}/aurorae/common${theme}/*                                                  ${AURORAE_DIR}/${name}${color}${theme}
+  cp -r ${SRC_DIR}/aurorae/common${theme}/*                                                  ${AURORAE_DIR}/${name}${color}-round${theme}
+  cp -r ${SRC_DIR}/color-schemes/${name}${ELSE_COLOR}${ELSE_THEME}.colors                    ${SCHEMES_DIR}
   cp -r ${SRC_DIR}/Kvantum/${name}${ELSE_THEME}                                              ${KVANTUM_DIR}
   cp -r ${SRC_DIR}/plasma/desktoptheme/${name}-dark                                          ${PLASMA_THEME}
   cp -r ${SRC_DIR}/plasma/desktoptheme/${name}-light/*                                       ${PLASMA_DIR}/${name}-light
   cp -r ${SRC_DIR}/plasma/desktoptheme/icons                                                 ${PLASMA_DIR}/${name}${color}
-  cp -r ${SRC_DIR}/color-schemes/${name}${ELSE_THEME}${ELSE_COLOR}.colors                    ${PLASMA_THEME}/colors
-  cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${theme}${color}       ${LOOKFEEL_DIR}
-  cp -r ${SRC_DIR}/wallpaper/${name}${theme}${color}                                         ${WALLPAPER_DIR}
+  cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}${theme}       ${LOOKFEEL_DIR}
+  cp -r ${SRC_DIR}/wallpaper/${name}${color}${theme}                                         ${WALLPAPER_DIR}
   mkdir -p                                                                                   ${PLASMA_THEME}/wallpapers
-  cp -r ${SRC_DIR}/wallpaper/${name}${theme}${color}                                         ${PLASMA_THEME}/wallpapers
+  cp -r ${SRC_DIR}/wallpaper/${name}${color}${theme}                                         ${PLASMA_THEME}/wallpapers
   cp -r ${SRC_DIR}/latte-dock/${name}.layout.latte                                           ${LATTE_THEME}
 }
 
 echo "Installing '${THEME_NAME} kde themes'..."
 
-for theme in "${themes[@]:-${THEME_VARIANTS[0]}}"; do
-  for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
-    install "${name:-${THEME_NAME}}" "${theme}" "${color}"
+for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
+  for theme in "${themes[@]:-${THEME_VARIANTS[@]}}"; do
+    install "${name:-${THEME_NAME}}" "${color}" "${theme}"
   done
 done
 
